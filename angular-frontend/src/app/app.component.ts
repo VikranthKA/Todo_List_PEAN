f59f1a8477e6fa0040c46f03c5d3521085c07826
vikranth <vikranthka@gmail.com>
2024-10-02 13:04:00 +0530

    working on scss

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/layout/header/header.component';
+import { SidenavbarComponent } from './components/sidenav/sidenavbar/sidenavbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent,SidenavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
